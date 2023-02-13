ff3d0f009e0e6456cd79682fcb86953320b484e1
pavansadhika <121276288+pavansadhika@users.noreply.github.com>
2023-02-13 22:15:03 +0530

    added

---
+++ src/main/java/SearchEngine.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchEngine {
+   public List<String> search(String query) {
+      // Implement the search functionality here
+      // For now, we will return a hardcoded list of results
+      List<String> results = new ArrayList<>();
+      results.add("Result 1");
+      results.add("Result 2");
+      results.add("Result 3");
+      return results;
+   }
+}
